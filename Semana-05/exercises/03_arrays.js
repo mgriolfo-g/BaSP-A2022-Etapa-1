67a3188b175be4beef643d45dfddc435b9c1a362
Martin Gabriel Riolfo <mgriolfo@gmail.com>
2022-09-20 00:38:11 -0300

    BaSP-W05-2022: completed file 03 in exercises folder

--- Semana-05/exercises/03_arrays.js
+++ Semana-05/exercises/03_arrays.js
@@ -10,7 +10,11 @@ console.log('**********Script start: 03_arrays.js**********');
 // Subtask start declaration on console
 console.log('-Subtask 3a:');
 
+var arrMonth = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre",
+    "Noviembre", "Diciembre"];
 
+// Subtask results print on console
+console.log('El mes 5 es ' + arrMonth[4] + ' y el mes 11 es ' + arrMonth[10] + '!');
 
 
 // b. Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).
@@ -18,7 +22,8 @@ console.log('-Subtask 3a:');
 // Subtask start declaration on console
 console.log('-Subtask 3b:');
 
-
+// Subtask results print on console
+console.log(arrMonth.sort());
 
 
 // c. Agregar un elemento al principio y al final del array (utilizar unshift y push).
@@ -26,7 +31,11 @@ console.log('-Subtask 3b:');
 // Subtask start declaration on console
 console.log('-Subtask 3c:');
 
+arrMonth.unshift('Aguinaldo');
+arrMonth.push('Bono Performance');
 
+// Subtask results print on console
+console.log(arrMonth);
 
 
 // d. Quitar un elemento del principio y del final del array (utilizar shift y pop).
@@ -34,7 +43,11 @@ console.log('-Subtask 3c:');
 // Subtask start declaration on console
 console.log('-Subtask 3d:');
 
+arrMonth.shift();
+arrMonth.pop();
 
+// Subtask results print on console
+console.log(arrMonth);
 
 
 // e. Invertir el orden del array (utilizar reverse).
@@ -42,7 +55,10 @@ console.log('-Subtask 3d:');
 // Subtask start declaration on console
 console.log('-Subtask 3e:');
 
+arrMonth.reverse();
 
+// Subtask results print on console
+console.log(arrMonth);
 
 
 // f. Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).
@@ -50,7 +66,10 @@ console.log('-Subtask 3e:');
 // Subtask start declaration on console
 console.log('-Subtask 3f:');
 
+var strArrMonth = arrMonth.join('-');
 
+// Subtask results print on console
+console.log(strArrMonth);
 
 
 // g. Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).
@@ -58,5 +77,10 @@ console.log('-Subtask 3f:');
 // Subtask start declaration on console
 console.log('-Subtask 3g:');
 
+var arrMonth2 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre",
+    "Noviembre", "Diciembre"];
 
+var arrMonth5to11 = arrMonth2.slice(4,11);
 
+// Subtask results print on console
+console.log(arrMonth5to11);
