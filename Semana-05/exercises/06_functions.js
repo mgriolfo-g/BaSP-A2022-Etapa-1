5a71affbe0ec5487defe8c4a61b8b5d890a55c23
Martin Gabriel Riolfo <mgriolfo@gmail.com>
2022-09-20 02:44:33 -0300

    BaSP-W05-2022: completed file 06 in exercises folder

--- Semana-05/exercises/06_functions.js
+++ Semana-05/exercises/06_functions.js
@@ -10,7 +10,14 @@ console.log('**********Script start: 06_functions.js**********');
 // Subtask start declaration on console
 console.log('-Subtask 6a:');
 
+function addNum(num3,num4) {
+    return num3 + num4;
+}
 
+var sum2 = addNum(3,5);
+
+// Subtask results print on console
+console.log(sum2);
 
 
 /*
@@ -22,7 +29,17 @@ console.log('-Subtask 6a:');
 // Subtask start declaration on console
 console.log('-Subtask 6b:');
 
+function addNum2(num5,num6) {
+    if ( isNaN(num5) || isNaN(num6) ) {
+        alert('addNum2 function exception: not a number parameter')
+        return 'Error';
+    }
+    return num5 + num6;
+}
 
+// Subtask results print on console
+console.log(addNum2(3,'five'));
+console.log(addNum2(3,5));
 
 
 // c. Aparte, crear una función validate Integer que reciba un número como parámetro y devuelva verdadero si es un
@@ -31,7 +48,13 @@ console.log('-Subtask 6b:');
 // Subtask start declaration on console
 console.log('-Subtask 6c:');
 
+function validateInteger(num7) {
+    return Number.isInteger(num7);
+}
 
+// Subtask results print on console
+console.log(validateInteger(3));
+console.log(validateInteger(3.5));
 
 
 /*
@@ -43,7 +66,28 @@ console.log('-Subtask 6c:');
 // Subtask start declaration on console
 console.log('-Subtask 6d:');
 
+function addNum3(num8,num9) {
+    if ( isNaN(num8) || isNaN(num9) ) {
+        alert('addNum3 function exception: not a number parameter');
+        return 'Error';
+    }
+    if (validateInteger(num8) == false) {
+        alert('addNum3 function alert: ' + num8 + ' is not an integer. Rounding to proceed');
+        num8 = Math.round(num8);
+    }
+    if (validateInteger(num9) == false) {
+        alert('addNum3 function alert: ' + num9 + ' is not an integer. Rounding to proceed');
+        num9 = Math.round(num9);
+    }
 
+    return num8 + num9;
+}
+
+// Subtask results print on console
+console.log(addNum3(3,'five'));
+console.log(addNum3(3,5));
+console.log(addNum3(3.4,5));
+console.log(addNum3(3.5,5.5));
 
 
 // e. Convertir la validación del ejercicio 6d) en una función separada y llamarla dentro de la función suma
@@ -52,5 +96,27 @@ console.log('-Subtask 6d:');
 // Subtask start declaration on console
 console.log('-Subtask 6e:');
 
+function roundNum(num) {
+    if (validateInteger(num) == false) {
+        alert('roundNum function alert: ' + num + ' is not an integer. Rounding to proceed');
+        num = Math.round(num);
+    }
+    return num;
+}
 
+function addNum4(num10,num11) {
+    if ( isNaN(num10) || isNaN(num11) ) {
+        alert('addNum4 function exception: not a number parameter');
+        return 'Error';
+    }
+    num10 = roundNum(num10);
+    num11 = roundNum(num11);
 
+    return num10 + num11;
+}
+
+// Subtask results print on console
+console.log(addNum4('five',3));
+console.log(addNum4(3,5));
+console.log(addNum4(3.4,5));
+console.log(addNum4(3.5,5.5));
